7664e851e28c87e9a75a625c30503a8c1ea2856f
ayanfeoluwa <oladoyinoluwatomisin11@gmail.com>
2022-10-11 15:44:00 +0100

    "write the find method in the webservices and find method api endpoints ."

--- src/main/java/com/semicolon/quotes_generator/service/web/WebQuoteService.java
+++ src/main/java/com/semicolon/quotes_generator/service/web/WebQuoteService.java
@@ -1,4 +1,12 @@
 package com.semicolon.quotes_generator.service.web;
 
+import com.semicolon.quotes_generator.data.model.WebQuote;
+import com.semicolon.quotes_generator.dtos.responses.LoadQuoteResponse;
+
 public interface WebQuoteService {
+    LoadQuoteResponse loadWebQuotesToDatabase();
+
+    WebQuote next();
+
+    WebQuote previous();
 }
