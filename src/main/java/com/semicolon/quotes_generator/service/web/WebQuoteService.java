a21360ceb02169351d64a7753a522b0c86a54cc8
Adewale Adeyinka <adeyinkawale13@gmail.com>
2022-10-11 13:02:05 +0100

    "added repository and service for web quote and web quote"

--- src/main/java/com/semicolon/quotes_generator/service/web/WebQuoteService.java
+++ src/main/java/com/semicolon/quotes_generator/service/web/WebQuoteService.java
@@ -1,4 +1,8 @@
 package com.semicolon.quotes_generator.service.web;
 
+import com.semicolon.quotes_generator.dtos.requests.CreateQuoteRequest;
+import com.semicolon.quotes_generator.dtos.responses.CreateQuoteResponse;
+
 public interface WebQuoteService {
+    CreateQuoteResponse loadWebQuote(CreateQuoteRequest createQuoteRequest);
 }
--- src/main/java/com/semicolon/quotes_generator/service/web/WebQuoteServiceImpl.java
+++ src/main/java/com/semicolon/quotes_generator/service/web/WebQuoteServiceImpl.java
@@ -1,8 +1,14 @@
 package com.semicolon.quotes_generator.service.web;
 
+import com.semicolon.quotes_generator.dtos.requests.CreateQuoteRequest;
+import com.semicolon.quotes_generator.dtos.responses.CreateQuoteResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WebQuoteServiceImpl implements WebQuoteService {
 
+    @Override
+    public CreateQuoteResponse loadWebQuote(CreateQuoteRequest createQuoteRequest) {
+        return null;
+    }
 }
