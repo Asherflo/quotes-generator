e9a655c80c6800379937ab00a00977bbcfb286a4
Asherflo <aniyikayetemitopeflorence@gmail.com>
2022-10-11 10:38:35 +0100

    initial commit

---
+++ src/main/java/com/semicolon/quotes_generator/QuotesGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.semicolon.quotes_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotesGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuotesGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/semicolon/quotes_generator/QuotesGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semicolon.quotes_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotesGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
